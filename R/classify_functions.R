3ba162271c84ef8243f3f065f9bc09ad60d9c796
hugomflavio <hmflavio92@gmail.com>
2025-02-22 23:28:58 -0400

    update classify function to work with topography object

--- R/classify_functions.R
+++ R/classify_functions.R
@@ -1,26 +1,28 @@
 #' break stress by quantiles
 #' 
 #' @param world a world object, e.g. the output of \code{\link{gen_world}}.
-#' @param breaks a named vector of quantile breaks to apply. The names are used
-#'   as labels. E.g. c("A" = 0, "B" = 0.5) would divide the lower half of the
-#'   world as "A", and the upper half of the world as "B".
+#' @param topography a topography table containing at least an id column and a
+#'   q column (with the minimum quantile for each terrain id).
 #' 
 #' @return an updated world object
 #' 
 #' @export
 #' 
-classify_heights <- function(world, breaks) {
-  stress <- quantile(world$map$stress, breaks)
+classify_heights <- function(world, topography) {
+
+  world$topography <- topography
+
+  stress <- quantile(world$map$stress, topography$q)
 
   world$map$topography <- NA
 
-  for (i in 1:length(breaks)) {
+  for (i in 1:nrow(topography)) {
     these <- world$map$stress >= stress[i]
-    world$map$topography[these] <- names(breaks)[i]
+    world$map$topography[these] <- topography$id[i]
   }
 
   world$map$topography <- factor(world$map$topography,
-                               levels = names(breaks))
+                               levels = topography$id)
 
   world$map$land <- TRUE
   world$map$land[grepl("water", world$map$topography)] <- FALSE
@@ -29,9 +31,11 @@ classify_heights <- function(world, breaks) {
   world$map$height <- world$map$height - min(world$map$height[world$map$land])
   world$map$height[world$map$height < 0] <- 0
 
-  report <- as.data.frame(rev(table(world$map$topography)))
+  report <- as.data.frame(table(world$map$topography))
   colnames(report) <- c("type", "n_tiles")
   report$pct <- round(report$n_tiles / sum(report$n_tiles) * 100, 2)
+
+  world$topography <- cbind(world$topography, report[, -1])
 
   # printing
   report <- capture.output(print(report, topn = nrow(report)))
